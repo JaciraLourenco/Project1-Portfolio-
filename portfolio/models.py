92cde5b09df1a917bb9b632cca9e5720be8b859f
JaciraLourenco <a22502992@alunos.ulht.pt>
2026-04-07 01:13:38 +0000

    Adicionar modelo Licenciatura com admin

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=200)
+    instituicao = models.CharField(max_length=200)
+    area = models.CharField(max_length=100)
+    duracao_anos = models.IntegerField()
+    descricao = models.TextField(blank=True)
+    link = models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.nome
